859b37fc1ab072288d9c737e9384f2a0e9945d04
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-17 21:20:36 +0200

    script.js file fixed to solve mail exercise

---
+++ js/mailscript.js
@@ -0,0 +1,20 @@
+const emailList = ["gino@gmail.com", "adinolfi@tiscali.it", "marioSconcerti@gmail.com", "pino@gmail.com"];
+
+let userEmail = prompt('inserisci la tua email')
+for (let i = 0 ; i < emailList.length ; i++) {
+    if (userEmail === emailList[i]){
+        console.log('email trovata e valida urrà')
+        i = (emailList.length - 1)
+        }
+    else if(userEmail !== emailList[i]){
+        
+        if (i == (emailList.length - 1 )){
+            console.log("email invalida")
+        }
+        else{
+            console.log('continuo a cercare')
+        }
+
+    }
+}
+  
