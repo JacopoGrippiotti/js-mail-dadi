08c057fc277e525d643965cf93d229e1b2c2e43d
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-17 21:28:17 +0200

    fixed mailscript.js file

--- js/mailscript.js
+++ js/mailscript.js
@@ -2,15 +2,18 @@ const emailList = ["gino@gmail.com", "adinolfi@tiscali.it", "marioSconcerti@gmai
 
 let userEmail = prompt('inserisci la tua email')
 for (let i = 0 ; i < emailList.length ; i++) {
-    if (userEmail === emailList[i]){
+    if (userEmail === emailList[i]) {
         console.log('email trovata e valida urrà')
         i = (emailList.length - 1)
         }
+    
     else if(userEmail !== emailList[i]){
         
         if (i == (emailList.length - 1 )){
+            
             console.log("email invalida")
         }
+        
         else{
             console.log('continuo a cercare')
         }
